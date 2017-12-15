d5323c60d32e913416620946d3ed55aac676bd64
LittleSoulstealer <33126896+LittleSoulstealer@users.noreply.github.com>
2017-12-15 09:33:36 +0100

    Add files via upload

---
+++ Silniki1.1/Assets/Scripts/GameController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameController : MonoBehaviour
+{
+
+
+    [SerializeField] private GameObject _Template;
+    [SerializeField] private int _BombCount = 3;
+    [SerializeField] private List<script1_bomb> _Bombs;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        CreatingBomb();
+        if (_BombCount == _Bombs.Count)
+        {
+            foreach (var b in _Bombs)
+            {
+                b.Activate();
+                
+            }
+            Destroy(gameObject);
+          
+        }
+        
+    }
+
+
+    void CreatingBomb()
+    {
+        if (_BombCount > _Bombs.Count)
+        {
+            if (Input.GetMouseButtonDown(0))
+
+            {
+                var point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                point.z = -5;
+                Debug.Log("Clicked");
+                GameObject bomb = Instantiate(_Template, point, Quaternion.identity)as GameObject;
+
+                var bombScript = bomb.GetComponent<script1_bomb>();
+
+                _Bombs.Add(bombScript);
+            }
+        }
+    }
+}
---
+++ Silniki1.1/Assets/Scripts/script1_bomb.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1_bomb : MonoBehaviour {
+
+    #region Inspected Variables
+    [SerializeField]private float _Radius=10.0f;
+    [SerializeField]private float _Time=3.0f;
+    [SerializeField]private float _Force=400.0f;
+    [SerializeField] private AudioSource _AudioSrc;
+    private float acc;
+
+    #endregion
+
+
+    #region Unity Methods
+
+
+	void Update () {
+
+    
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,_Radius);
+
+    }
+
+    #endregion
+
+    #region PrivateMethods
+    public void Activate()
+    {
+        _AudioSrc.Play();
+        Destroy(gameObject,0.5f);
+        var colliders = Physics2D.OverlapCircleAll(transform.position, _Radius);
+  
+
+        foreach(var collider in colliders)
+        {
+            var rb = collider.GetComponent<Rigidbody2D>();
+            if (rb == null)
+            {
+                continue;
+            }
+
+            
+            var vector = collider.transform.position - transform.position;
+            var direction = vector.normalized;
+            var lenght = vector.magnitude;
+
+       
+            var force = direction *Mathf.Pow(1.0f/(lenght+1.0f), 2.0f)* _Force;
+
+            rb.AddForce(force, ForceMode2D.Impulse);
+        }
+    }
+#endregion
+}
